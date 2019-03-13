e7b2fc1c71f87fb0f3035cc3eedca0baa7f3a3fc
sadegh <sadeghte@gmail.com>
2019-03-13 22:19:15 +0330

    email validator and user email update.

--- src/validator/index.js
+++ src/validator/index.js
@@ -1,11 +1,13 @@
 import locationHistory from './locationHistory';
 import objectId from './objectId';
 import string from './string';
+import email from './email';
 
-export {locationHistory, objectId, string};
+export {locationHistory, objectId, string, email};
 
 export default {
   locationHistory,
   objectId,
-  string
+  string,
+  email
 }
