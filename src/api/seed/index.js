9fb4d3fe2b3acad7266f48ff97b2124ee5e197d1
sadegh <sadeghte@gmail.com>
2019-04-05 00:22:22 +0430

    all seed methods enabled.

--- src/api/seed/index.js
+++ src/api/seed/index.js
@@ -15,21 +15,21 @@ const nacl = require('tweetnacl');
 
 router.all('/', function (req, res, next) {
   initTokens.map(token => {new Token(token).save();});
-  // initCountries.map(country => {new Country(country).save();});
-  // initPaymentMethods.map(method => {new PaymentMethod(method).save();});
-  // // initialize new 20 test wallets;
-  // new Array(20).fill(0)
-  //     .map(n => nacl.sign.keyPair())
-  //     .map(keyPair => ({
-  //       publicKey: "0x"+Buffer.from(keyPair.publicKey).toString('hex'),
-  //       secretKey: "0x"+Buffer.from(keyPair.secretKey).toString('hex'),
-  //     }))
-  //     .map(keyPair => {(new Wallet(keyPair)).save();});
-  // initCurrencies.map(c => {
-  //   if(!c.title)
-  //     c.title = c.code;
-  //   new Currency(c).save();
-  // });
+  initCountries.map(country => {new Country(country).save();});
+  initPaymentMethods.map(method => {new PaymentMethod(method).save();});
+  // initialize new 20 test wallets;
+  new Array(20).fill(0)
+      .map(n => nacl.sign.keyPair())
+      .map(keyPair => ({
+        publicKey: "0x"+Buffer.from(keyPair.publicKey).toString('hex'),
+        secretKey: "0x"+Buffer.from(keyPair.secretKey).toString('hex'),
+      }))
+      .map(keyPair => {(new Wallet(keyPair)).save();});
+  initCurrencies.map(c => {
+    if(!c.title)
+      c.title = c.code;
+    new Currency(c).save();
+  });
   res.send({
     success: true,
     message: 'feed successfully done.'
